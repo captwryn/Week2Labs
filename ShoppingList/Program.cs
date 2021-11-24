c2e92c2af7fbe232c902e3d43c28104d71be7591
captwryn <93493592+captwryn@users.noreply.github.com>
2021-11-24 11:17:59 -0500

    updated project 2

--- ShoppingList/Program.cs
+++ ShoppingList/Program.cs
@@ -5,6 +5,57 @@ namespace DictionaryDemo
 {
     class Program
     {
+       
+        static void WelcomeMenu(Dictionary<string, decimal> glist)
+        {
+            foreach (var pair in glist)
+            {
+                Console.WriteLine($"      {pair.Key,-12} ${pair.Value}");
+            }
+        }
+        static void AddtoCart(Dictionary<string, decimal> glist, Dictionary<string, decimal> basket)
+        {
+            bool adding = true;
+            do
+            {
+                Console.WriteLine("\n  What would you like to add something to your basket?:\n   (enter item or 'done' to finish; any other entries are ignored)");
+                string entry = Console.ReadLine().ToLower();
+
+                if (glist.ContainsKey(entry))
+                {
+                    basket.Add(entry, glist[entry]);
+                    Console.WriteLine($"Added {entry} to your basket for {glist[entry]}");
+                }
+                else if (entry == "done")
+                {
+                    adding = false;
+                }
+                else if (entry == "menu")
+                {
+                    WelcomeMenu(glist);
+                }
+                else
+                {
+                    Console.WriteLine("We don't have that item.  (enter 'menu' to see the menu)");
+                }
+            } while (adding);
+        }
+        static void PrintReceipt(Dictionary<string, decimal> basket)
+        {
+            decimal total = 0;
+
+            Console.WriteLine("\n---------------------------\n");
+            Console.WriteLine("   Your basket has:\n");
+            foreach (var pair in basket)
+            {
+                Console.WriteLine($"      {pair.Key,-12} ${pair.Value}");
+                total += pair.Value;
+            }
+            Console.WriteLine($"\n   Your total cost is:\n   ${total,20}");
+            Console.WriteLine("\n---------------------------");
+        }
+
+
         static void Main(string[] args)
         {
             Dictionary<string, decimal> glist = new Dictionary<string, decimal>();
@@ -17,43 +68,15 @@ namespace DictionaryDemo
             glist["bread"] = 1.50m;
             glist["rice"] = 6.00m;
 
-            decimal total = 0;
             int amount = 0;
             Dictionary<string, decimal> basket = new Dictionary<string, decimal>();
-            bool adding = true;
 
             Console.WriteLine("  Welcome to the store\n\n  Here's a list of available items:");
-            foreach (var pair in glist)
-            {
-                Console.WriteLine($"      {pair.Key,-12} ${pair.Value}");
-            }
-            do
-            {
-                Console.WriteLine("\n  What would you like to add something to your basket?:\n   (enter item or done to finish; any other entries are ignored)");
-                string entry = Console.ReadLine().ToLower();
-                
-                foreach (var pair in glist)
-                {
-                    if (entry == pair.Key)
-                    {
-                        basket[pair.Key] = pair.Value;
-                    }
-                    if (entry == "done")
-                    {
-                        adding = false;
-                    }
-                }
-            } while (adding);
+            WelcomeMenu(glist);
+            
+            AddtoCart(glist, basket);
 
-            Console.WriteLine("\n---------------------------\n");
-            Console.WriteLine("   Your basket has:\n");
-            foreach (var pair in basket)
-            {
-                Console.WriteLine($"      {pair.Key, -12} ${pair.Value}");
-                total = total + pair.Value;
-            }
-            Console.WriteLine($"\n   Your total cost is:\n   ${total,20}");
-            Console.WriteLine("\n---------------------------");
+            PrintReceipt(basket);
         }
     }
 }
